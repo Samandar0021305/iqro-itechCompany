e1fefc6853377e77f1a954db76926aac80fc72df
Eljahon <neljahon@gmail.com>
2022-11-23 17:03:53 +0500

    list add

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
-
+import { appWithTranslation } from 'next-i18next';
+import Headers from '../components/Header/header'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Headers/>
+  <Component {...pageProps} />
+  </>
 }
 
-export default MyApp
+export default appWithTranslation(MyApp)
