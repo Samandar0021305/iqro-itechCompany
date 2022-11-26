d6d9f8aac992ce18c757cfae143ee60fae7cc890
AbdumalikovIqboljon <happinessjonabdumalikov@gmail.com>
2022-11-26 19:31:18 +0500

    feat: create reusable button component

--- pages/user/index.jsx
+++ pages/user/index.jsx
@@ -1,10 +1,13 @@
+import BaseButton from "../../components/BaseButton/BaseButton.jsx";
 function User() {
-          return ( <>
-          <div className="main">
-                    <span className="text-3xl">user.</span>
-          </div>
-          
-          </> );
+  return (
+    <>
+      <div className="main">
+        <span className="text-3xl">user.</span>
+        <BaseButton>Contact</BaseButton>
+      </div>
+    </>
+  );
 }
 
 export default User;
