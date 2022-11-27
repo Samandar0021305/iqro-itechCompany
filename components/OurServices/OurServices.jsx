1e763d4ba8dde43799b0a69965c2b28c380d7920
Samandar.log <TSamandardev@gmail.com>
2022-11-27 16:45:59 +0500

    commit

--- components/OurServices/OurServices.jsx
+++ components/OurServices/OurServices.jsx
@@ -13,7 +13,7 @@ const OurServices = () => {
           <ul className='flex flex-wrap  justify-between items-center relative'>
              {
               Services.map((postEl,index)=>{
-                return <li key={index} className='lg:w-96 pl-1 pr-1 py-2 md:mr-1 ml-1 lg:p-6 lg:h-64 md:w-64 md:h-32 sm:w-40 sm:h-32 w-36  pl-1 pr-1  h-32 mt-3 flex justify-between bg-blue bg-opacity-20'>
+                return <li key={index} className='lg:w-96   md:mr-1 ml-1 lg:p-6 lg:h-64 md:w-64 md:h-32 sm:w-40 sm:h-32 w-36  pl-2 pr-2  h-32 mt-3 flex justify-between bg-blue bg-opacity-20'>
                   <p className='lg:text-base lg:font-medium font-normal text-xs h-auto lg:w-40 w-24'>{t(postEl.post)}</p>
                   <Image  src={postEl.img}  className="lg:w-48 lg:h-48 w-16 h-16 sm:mt-0 mt-3"  alt=""  />
                 </li>
