906c4ba023f659f4c1e18d55167fe23b8e523570
Samandar.log <TSamandardev@gmail.com>
2022-11-28 12:44:41 +0500

    init

--- components/DevelopmentMobile/DevelopmentMobile.jsx
+++ components/DevelopmentMobile/DevelopmentMobile.jsx
@@ -11,7 +11,7 @@ function DevelopmentMobile() {
        <h2 className='text-blue font-medium md:text-[67px] text-[28px]'>Development of <br/> mobile applications</h2>
        </div>
 
-       <div className='lg:bg-blue md:bg-blue bg-white md:bg-opacity-20  pr-6 pl-6  lg:bg-opacity-20 mt-[30px] sm:mt-[0]  pb-[70px]'>
+       <div className='lg:bg-blue sm:bg-blue bg-white sm:bg-opacity-20  pr-6 pl-6  lg:bg-opacity-20 mt-[30px] sm:mt-[0]  pb-[70px]'>
               <div className=' container lg:pr-1 lg:pl-1 flex justify-between  lg:flex-row  md:flex-row sm:flex-col-reverse xsm:flex-col-reverse flex-col-reverse p-0 mx-auto py-5'>
                 <div className='w-[100%] sm:w-[50%]'>
                     <p className='lg:text-[28px] text-[16px] mt-[20px] md:mt-0 font-normal'>In collaboration with startups, we have
