715a9fb9366f2644c68661e0410814f180ba7f5a
Samandar.log <TSamandardev@gmail.com>
2022-11-28 12:42:47 +0500

    commits

--- components/OurServices/OurServices.jsx
+++ components/OurServices/OurServices.jsx
@@ -10,13 +10,13 @@ const OurServices = () => {
   return (
     <div className='container lg:pr-1 lg:pl-1  p-0 mx-auto py-5 '>
           <h2 className='font-medium lg:text-start text-center lg:text-6xl  text-3xl text-blue'>Our services</h2>
-          <ul className='flex justify-between flex-wrap items-center' >
+          <ul className='flex justify-between flex-wrap items-center ' >
             {
               Services.map((postEl,id)=>{
                 return (
-                  <li key={id} className="lg:w-[410px] p-2 lg:h-[250px] md:w-[220px] md:h-[135px] sm:w-[162px] sm:h-[135px] w-[162px] h-[135px] flex mt-[20px] justify-between  bg-bluef">
-                    <p className='lg:text-[21px] text-[12px]'>{t(postEl.post)}</p> 
-                         <Image  className='block object-scale-down' src={postEl.img} alt=""  />
+                  <li key={id} className="lg:w-[410px] p-2 lg:h-[250px] relative md:w-[220px] md:h-[135px] sm:w-[162px] sm:h-[135px] w-[152px] h-[135px] flex mt-[20px]  bg-bluef">
+                    <p className='lg:text-[21px] text-[12px] w-[70%]'>{t(postEl.post)}</p> 
+                         <Image  className='block object-scale-down  absolute bottom-[0] right-0' src={postEl.img} alt=""  />
                   </li>
                 )
               })
