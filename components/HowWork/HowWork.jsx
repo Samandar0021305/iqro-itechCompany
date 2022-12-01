5f49cc3bbb8cee4e4a7913bf65a71b6458e4527e
Samandar.log <TSamandardev@gmail.com>
2022-12-01 15:57:41 +0500

    commits

--- components/HowWork/HowWork.jsx
+++ components/HowWork/HowWork.jsx
@@ -10,11 +10,11 @@ const HowWork = () => {
   return (
     <div className='container px-3 mt-[60px] mb-[20px] mx-auto py-12'>
       <PageTitle title="how we work"/>
-      <ul className="flex  break-after-auto   md:justify-around justify-start flex-col items-start md:flex-row mt-5 md:mt-16 ">
+      <ul className="flex  break-after-auto   md:justify-between justify-start flex-col items-start md:flex-row mt-5 md:mt-16 ">
         {
           WorkedList.map((post, id) => {
             return (
-              <li key={id} className="flex  md:mt-2 mt-6   justify-center items-center md:flex-col flex-row">
+              <li key={id} className="flex  md:mt-2 mt-6  md:w-[20%]  justify-center items-center md:flex-col flex-row">
                 <Image src={post.img} className="text-center md:ml-3" alt="" />
                 <span className="flex flex-col ml-5">
                   <h4 className='md:text-center md:mt-1 mb-1   md:text-[21px] text-[16px] text-blackDev'>{t(post.title)}</h4>
